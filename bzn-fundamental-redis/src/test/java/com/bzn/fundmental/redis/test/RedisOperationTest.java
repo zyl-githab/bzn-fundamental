3ae0222188f79938399a76650644c537b182af2f
fengli <fengli@baozhunniu.com>
2016-11-23 17:23:04 +0800

    修改httpclient

--- bzn-fundamental-redis/src/test/java/com/bzn/fundmental/redis/test/RedisOperationTest.java
+++ bzn-fundamental-redis/src/test/java/com/bzn/fundmental/redis/test/RedisOperationTest.java
@@ -17,23 +17,27 @@ public class RedisOperationTest {
 
 	@Test
 	public void testSet() {
-		redisOperations.set("sync_data_switch","1",-1L);
-		redisOperations.delete("sync_data_offset");
-		redisOperations.delete("global_policy_id_offset");
+		Long s = System.currentTimeMillis();
+		for (int i = 1; i < 1000; i++) {
+			redisOperations.setNx("sync_data_switch", "1");
+		}
+		System.out.println(System.currentTimeMillis() - s);
 	}
 
 	@Test
 	public void testGet() {
 		System.out.println(redisOperations.get("sync_data_offset"));
-	/*	redisOperations.delete("sync_data_offset");
-	 	redisOperations.delete("open_policy_id_offset");
-		redisOperations.delete("baidu_pack_policy_id_offset");
-		redisOperations.delete("official_policy_id_offset");
-		redisOperations.delete("open_policy_id_offset_set");
-		redisOperations.delete("open_policy_id_offset_hash");
-		redisOperations.delete("open_policy_id_offset_lock");
-		redisOperations.delete("open_policy_id_old_offset");*/
-	//	redisOperations.set("open_policy_id_offset", "2016-06-01 00:00:01");
+		/*
+		 * redisOperations.delete("sync_data_offset");
+		 * redisOperations.delete("open_policy_id_offset");
+		 * redisOperations.delete("baidu_pack_policy_id_offset");
+		 * redisOperations.delete("official_policy_id_offset");
+		 * redisOperations.delete("open_policy_id_offset_set");
+		 * redisOperations.delete("open_policy_id_offset_hash");
+		 * redisOperations.delete("open_policy_id_offset_lock");
+		 * redisOperations.delete("open_policy_id_old_offset");
+		 */
+		// redisOperations.set("open_policy_id_offset", "2016-06-01 00:00:01");
 		System.out.println("00");
 	}
 }
