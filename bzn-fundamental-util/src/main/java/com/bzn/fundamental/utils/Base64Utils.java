409f6d4f9b016eca82b876fd3d1753b62b7ce139
zhuyanlong <zhuyanlong@baozhunniu.com>
2017-02-20 10:50:30 +0800

    修改base64方法

--- bzn-fundamental-util/src/main/java/com/bzn/fundamental/utils/Base64Utils.java
+++ bzn-fundamental-util/src/main/java/com/bzn/fundamental/utils/Base64Utils.java
@@ -1,272 +1,102 @@
 package com.bzn.fundamental.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 /**
- * 描述: Base64处理工具类, 采用Unicode编码</br>
+ * 描述: Base64工具类, 该工具类提供了将对象转换成Base64编码的方法和将Base64编码转换成对象的方法</br>
  * 
  * @author Shangxp
  * @version 1.0.0
- * @date 2016年7月28日 下午6:40:13
+ * @date 2016.03.23
  * 
  * Copyright © 2016 BZN Corporation, All Rights Reserved.
  */
+@SuppressWarnings("restriction")
 public class Base64Utils {
-	
-	// 字符编码
-	public static final String UNICODE = "Unicode";
-	
-	// 编码表
-	private static final byte[] encodingTable = { (byte) 'A', (byte) 'B',
-		(byte) 'C', (byte) 'D', (byte) 'E', (byte) 'F', (byte) 'G',
-		(byte) 'H', (byte) 'I', (byte) 'J', (byte) 'K', (byte) 'L',
-		(byte) 'M', (byte) 'N', (byte) 'O', (byte) 'P', (byte) 'Q',
-		(byte) 'R', (byte) 'S', (byte) 'T', (byte) 'U', (byte) 'V',
-		(byte) 'W', (byte) 'X', (byte) 'Y', (byte) 'Z', (byte) 'a',
-		(byte) 'b', (byte) 'c', (byte) 'd', (byte) 'e', (byte) 'f',
-		(byte) 'g', (byte) 'h', (byte) 'i', (byte) 'j', (byte) 'k',
-		(byte) 'l', (byte) 'm', (byte) 'n', (byte) 'o', (byte) 'p',
-		(byte) 'q', (byte) 'r', (byte) 's', (byte) 't', (byte) 'u',
-		(byte) 'v', (byte) 'w', (byte) 'x', (byte) 'y', (byte) 'z',
-		(byte) '0', (byte) '1', (byte) '2', (byte) '3', (byte) '4',
-		(byte) '5', (byte) '6', (byte) '7', (byte) '8', (byte) '9',
-		(byte) '+', (byte) '/' };
-	
-	// 解码表
-	private static final byte[] decodingTable;
-	
-	static {
-		decodingTable = new byte[128];
-		for (int i = 0; i < 128; i++) {
-			decodingTable[i] = (byte) -1;
-		}
-		for (int i = 'A'; i <= 'Z'; i++) {
-			decodingTable[i] = (byte) (i - 'A');
-		}
-		for (int i = 'a'; i <= 'z'; i++) {
-			decodingTable[i] = (byte) (i - 'a' + 26);
-		}
-		for (int i = '0'; i <= '9'; i++) {
-			decodingTable[i] = (byte) (i - '0' + 52);
-		}
-		decodingTable['+'] = 62;
-		decodingTable['/'] = 63;
-	}
 
+	public static String base64Encode(Serializable ser) throws IOException {
+		// 该流与内存中的一个字节数组关联
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		// 将字节数组流与ObjectOutputStream流对接
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
+		// 将对象输出, 最终对象的字节"流"到了字节数组中
+		oos.writeObject(ser);
+		oos.flush();
+		oos.close();
+		// 从流中取出字节数组
+		byte[] bytes = bos.toByteArray();
+		bos.close();
+		
+		// String result = new String(bytes, "ISO-8859-1");
+		
+		// 将字节编码成Base64字符串
+		BASE64Encoder encoder = new BASE64Encoder();
+		String result = encoder.encode(bytes);
+		
+		// 默认生成了有换行和回车, 需要去掉, 否则在Servlet中读取的时候会产生问题
+		result = result.replaceAll("\\r", "");
+		result = result.replaceAll("\\n", "");
+		
+		return result;
+	}
+	
+	public static Object base64Decode(String base64Str) throws IOException, ClassNotFoundException {
+		// 将字符串还原成字节
+		BASE64Decoder decoder = new BASE64Decoder();
+		byte[] bytes = decoder.decodeBuffer(base64Str);
+		// 使用字节数组输入流读取字节数组
+		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+		
+		// ByteArrayInputStream bis = new ByteArrayInputStream(base64Str.getBytes("ISO-8859-1"));
+		
+		// 与对象输入流对接
+		ObjectInputStream ois = new ObjectInputStream(bis);
+		Object object = ois.readObject();
+		ois.close();
+		bis.close();
+		
+		return object;
+	}
+	
 	/**
-	 * 使用Base64编码
+	 * 对URL中的参数进行UTF-8编码
 	 */
-	public static byte[] encode(byte[] data, int offset) {
-		byte[] bytes;
-		int realCount = data.length - offset;
-		int modulus = realCount % 3;
-		if (modulus == 0) {
-			bytes = new byte[(4 * realCount) / 3];
-		} else {
-			bytes = new byte[4 * ((realCount / 3) + 1)];
-		}
-		int dataLength = (data.length - modulus);
-		int a1;
-		int a2;
-		int a3;
-		for (int i = offset, j = 0; i < dataLength; i += 3, j += 4) {
-			a1 = data[i] & 0xff;
-			a2 = data[i + 1] & 0xff;
-			a3 = data[i + 2] & 0xff;
-			bytes[j] = encodingTable[(a1 >>> 2) & 0x3f];
-			bytes[j + 1] = encodingTable[((a1 << 4) | (a2 >>> 4)) & 0x3f];
-			bytes[j + 2] = encodingTable[((a2 << 2) | (a3 >>> 6)) & 0x3f];
-			bytes[j + 3] = encodingTable[a3 & 0x3f];
-		}
-		int b1;
-		int b2;
-		int b3;
-		int d1;
-		int d2;
-		switch (modulus) {
-		case 0: /* nothing left to do */
-			break;
-		case 1:
-			d1 = data[data.length - 1] & 0xff;
-			b1 = (d1 >>> 2) & 0x3f;
-			b2 = (d1 << 4) & 0x3f;
-			bytes[bytes.length - 4] = encodingTable[b1];
-			bytes[bytes.length - 3] = encodingTable[b2];
-			bytes[bytes.length - 2] = (byte) '=';
-			bytes[bytes.length - 1] = (byte) '=';
-			break;
-		case 2:
-			d1 = data[data.length - 2] & 0xff;
-			d2 = data[data.length - 1] & 0xff;
-			b1 = (d1 >>> 2) & 0x3f;
-			b2 = ((d1 << 4) | (d2 >>> 4)) & 0x3f;
-			b3 = (d2 << 2) & 0x3f;
-			bytes[bytes.length - 4] = encodingTable[b1];
-			bytes[bytes.length - 3] = encodingTable[b2];
-			bytes[bytes.length - 2] = encodingTable[b3];
-			bytes[bytes.length - 1] = (byte) '=';
-			break;
-		}
-		return bytes;
-	}
-
-	public static byte[] decode(byte[] data) {
-		byte[] bytes;
-		byte b1;
-		byte b2;
-		byte b3;
-		byte b4;
-		data = discardNonBase64Bytes(data);
-		if (data[data.length - 2] == '=') {
-			bytes = new byte[(((data.length / 4) - 1) * 3) + 1];
-		} else if (data[data.length - 1] == '=') {
-			bytes = new byte[(((data.length / 4) - 1) * 3) + 2];
-		} else {
-			bytes = new byte[((data.length / 4) * 3)];
-		}
-		for (int i = 0, j = 0; i < (data.length - 4); i += 4, j += 3) {
-			b1 = decodingTable[data[i]];
-			b2 = decodingTable[data[i + 1]];
-			b3 = decodingTable[data[i + 2]];
-			b4 = decodingTable[data[i + 3]];
-			bytes[j] = (byte) ((b1 << 2) | (b2 >> 4));
-			bytes[j + 1] = (byte) ((b2 << 4) | (b3 >> 2));
-			bytes[j + 2] = (byte) ((b3 << 6) | b4);
-		}
-		if (data[data.length - 2] == '=') {
-			b1 = decodingTable[data[data.length - 4]];
-			b2 = decodingTable[data[data.length - 3]];
-			bytes[bytes.length - 1] = (byte) ((b1 << 2) | (b2 >> 4));
-		} else if (data[data.length - 1] == '=') {
-			b1 = decodingTable[data[data.length - 4]];
-			b2 = decodingTable[data[data.length - 3]];
-			b3 = decodingTable[data[data.length - 2]];
-			bytes[bytes.length - 2] = (byte) ((b1 << 2) | (b2 >> 4));
-			bytes[bytes.length - 1] = (byte) ((b2 << 4) | (b3 >> 2));
-		} else {
-			b1 = decodingTable[data[data.length - 4]];
-			b2 = decodingTable[data[data.length - 3]];
-			b3 = decodingTable[data[data.length - 2]];
-			b4 = decodingTable[data[data.length - 1]];
-			bytes[bytes.length - 3] = (byte) ((b1 << 2) | (b2 >> 4));
-			bytes[bytes.length - 2] = (byte) ((b2 << 4) | (b3 >> 2));
-			bytes[bytes.length - 1] = (byte) ((b3 << 6) | b4);
-		}
-		return bytes;
-	}
-
-	/**
-	 * 使用Base64解码
-	 */
-	public static byte[] decode(String data) {
-		byte[] bytes;
-		byte b1;
-		byte b2;
-		byte b3;
-		byte b4;
-		data = discardNonBase64Chars(data);
-		if (data.charAt(data.length() - 2) == '=') {
-			bytes = new byte[(((data.length() / 4) - 1) * 3) + 1];
-		} else if (data.charAt(data.length() - 1) == '=') {
-			bytes = new byte[(((data.length() / 4) - 1) * 3) + 2];
-		} else {
-			bytes = new byte[((data.length() / 4) * 3)];
-		}
-		for (int i = 0, j = 0; i < (data.length() - 4); i += 4, j += 3) {
-			b1 = decodingTable[data.charAt(i)];
-			b2 = decodingTable[data.charAt(i + 1)];
-			b3 = decodingTable[data.charAt(i + 2)];
-			b4 = decodingTable[data.charAt(i + 3)];
-			bytes[j] = (byte) ((b1 << 2) | (b2 >> 4));
-			bytes[j + 1] = (byte) ((b2 << 4) | (b3 >> 2));
-			bytes[j + 2] = (byte) ((b3 << 6) | b4);
-		}
-		if (data.charAt(data.length() - 2) == '=') {
-			b1 = decodingTable[data.charAt(data.length() - 4)];
-			b2 = decodingTable[data.charAt(data.length() - 3)];
-			bytes[bytes.length - 1] = (byte) ((b1 << 2) | (b2 >> 4));
-		} else if (data.charAt(data.length() - 1) == '=') {
-			b1 = decodingTable[data.charAt(data.length() - 4)];
-			b2 = decodingTable[data.charAt(data.length() - 3)];
-			b3 = decodingTable[data.charAt(data.length() - 2)];
-			bytes[bytes.length - 2] = (byte) ((b1 << 2) | (b2 >> 4));
-			bytes[bytes.length - 1] = (byte) ((b2 << 4) | (b3 >> 2));
-		} else {
-			b1 = decodingTable[data.charAt(data.length() - 4)];
-			b2 = decodingTable[data.charAt(data.length() - 3)];
-			b3 = decodingTable[data.charAt(data.length() - 2)];
-			b4 = decodingTable[data.charAt(data.length() - 1)];
-			bytes[bytes.length - 3] = (byte) ((b1 << 2) | (b2 >> 4));
-			bytes[bytes.length - 2] = (byte) ((b2 << 4) | (b3 >> 2));
-			bytes[bytes.length - 1] = (byte) ((b3 << 6) | b4);
-		}
-		for (int i = 0; i < bytes.length; i++)
-			System.out.println("," + bytes[i]);
-		return bytes;
-	}
-
-	private static byte[] discardNonBase64Bytes(byte[] data) {
-		byte[] temp = new byte[data.length];
-		int bytesCopied = 0;
-		for (int i = 0; i < data.length; i++) {
-			if (isValidBase64Byte(data[i])) {
-				temp[bytesCopied++] = data[i];
-			}
-		}
-		byte[] newData = new byte[bytesCopied];
-		System.arraycopy(temp, 0, newData, 0, bytesCopied);
-		return newData;
-	}
-
-	private static String discardNonBase64Chars(String data) {
-		StringBuffer sb = new StringBuffer();
-		int length = data.length();
-		for (int i = 0; i < length; i++) {
-			if (isValidBase64Byte((byte) (data.charAt(i)))) {
-				sb.append(data.charAt(i));
-			}
-		}
-		return sb.toString();
-	}
-
-	private static boolean isValidBase64Byte(byte b) {
-		if (b == '=') {
-			return true;
-		} else if ((b < 0) || (b >= 128)) {
-			return false;
-		} else if (decodingTable[b] == -1) {
-			return false;
-		}
-		return true;
-	}
-
-	public static String encode(String data, String charset) throws Exception {
-		// byte[] result = (data.getBytes("Unicode"));
-		if (data == null || data.length() == 0)
-			return data;
-		int offset = 0;
-		// getBytes("unicode")转完后会在前头加上两字节"FE"
-		byte[] result = encode(data.getBytes(charset), offset);
-		StringBuffer sb = new StringBuffer(result.length);
-		for (int i = 0; i < result.length; i++)
-			sb.append((char) result[i]);
-		return sb.toString();
-	}
-
-	public static String decode(String data, String charset) throws Exception {
-		if (data == null || data.length() == 0)
-			return data;
-		return new String(decode(data), charset);
+	public static String urlEncodeUTF8(String parameter) throws UnsupportedEncodingException {
+		parameter = parameter.replaceAll("\\+", "-");
+		return URLEncoder.encode(parameter, "UTF-8");
 	}
 	
-	public static String replace(String data) {
-		return data.replace("/", "");
+	/**
+	 * 对URL中的参数进行UTF-8解码
+	 */
+	public static String urlDecodeUTF8(String parameter) throws UnsupportedEncodingException {
+		return URLDecoder.decode(parameter, "UTF-8").replaceAll("\\-", "+");
 	}
-
-	public static void main(String[] args) throws Exception {
-		String data = "我们";
-		String data1 = encode(data, UNICODE);
-		String data2 = decode(data1, UNICODE);
-		System.out.println(data);
-		System.out.println(data1);
-		System.out.println(data2);
+	
+	/**
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		Order order = new Order();
+		order.setOrderCode("1500058040020077");
+		order.setRemark("官网订单");
+		
+		// 输出编码后的结果
+		String base64Str = base64Encode(order);
+		System.out.println(base64Str);
+		
+		// 输出解码后的结果
+		Order o = (Order) base64Decode(base64Str);
+		System.out.println(o.getOrderCode() + "\t" + o.getRemark());
 	}
+	*/
 }
